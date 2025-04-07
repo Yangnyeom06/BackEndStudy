8552f96d223c5aab3880a09e6dec78e3a9ecbd6e
yangnyeom06 <lyujin1204@gmail.com>
2025-04-07 13:37:49 +0900

    upload

---
+++ src/main/kotlin/study/study/common/status/EnumStatus.kt
@@ -0,0 +1,23 @@
+package study.study.common.status
+
+/*
+enum class Gender(val desc: String) {
+    MAN("남"),
+    WOMAN("여")
+}
+*/
+
+enum class DormitoryType(val desc: String) {
+    GOUN_A("고운A"),
+    GOUN_B("고운B"),
+    GOUN_C("고운C"),
+    GYEONGSANG_11("경상11"),
+    GYEONGSANG_12("경상12"),
+    GYEONGSANG_13("경상13"),
+    GYEONGSANG_14("경상14")
+}
+
+enum class ResultCode(val msg: String) {
+    SUCCESS("정상처리 되었습니다."),
+    ERROR("에러가 발생했습니다.")
+}
---
+++ src/main/kotlin/study/study/member/entitiy/MemberEntities.kt
@@ -0,0 +1,42 @@
+package study.study.member.entitiy
+
+//import study.study.common.status.Gender
+import study.study.common.status.DormitoryType
+import jakarta.persistence.*
+import java.time.LocalDate
+
+@Entity
+@Table(
+    uniqueConstraints = [UniqueConstraint(name = "uk_member_login_id", columnNames = ["loginId"])]
+)
+class Member (
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    var id: Long? = null,
+
+    @Column(nullable = false, length = 30, updatable = false)
+    val loginId: String,
+
+    @Column(nullable = false, length = 100)
+    val password: String,
+
+    @Column(nullable = false, length = 10)
+    val name: String,
+
+    @Column(nullable = false, length = 10)
+    @Enumerated(EnumType.STRING)
+    val dormitory: DormitoryType,
+
+    /*
+      @Column(nullable = false)
+      @Temporal(TemporalType.DATE)
+      val birthDate: LocalDate,
+
+      @Column(nullable = false, length = 5)
+      @Enumerated(EnumType.STRING)
+      val gender: Gender,
+     */
+
+    @Column(nullable = false, length = 30)
+    val email: String,
+)
